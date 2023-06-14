c979f67e2e361ef1ce20ed67433de0b1f4a6b604
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-06-14 10:09:24 -0300

    Update hypercore.rs

--- lib/rust/hypercore.rs
+++ lib/rust/hypercore.rs
@@ -1,5 +1,24 @@
-use std::hypercore::hypercore
+use std::io::{Read, Write};
+use hypercore::Feed;
 
-let message = core.lenght::new();
-let message = core.contiguouslength::new();
-let message = core.fork::new();
+pub struct HypercoreLibrary {
+    feed: Feed,
+}
+
+impl HypercoreLibrary {
+    pub fn new(feed: Feed) -> Self {
+        HypercoreLibrary { feed }
+    }
+
+    pub fn read_data(&mut self, start: usize, length: usize) -> Result<Vec<u8>, hypercore::Error> {
+        let mut buf = vec![0u8; length];
+        self.feed.read(start, &mut buf)?;
+        Ok(buf)
+    }
+
+    pub fn append_data(&mut self, data: &[u8]) -> Result<(), hypercore::Error> {
+        self.feed.append(data)?;
+        Ok(())
+    }
+}
+
