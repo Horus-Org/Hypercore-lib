079a2a08f72cff1327ba7654d43c900b553d28ec
Rsync⚡️ <135646455+Rsync25@users.noreply.github.com>
2023-06-19 07:46:21 -0300

    Update hyeperbee.rs

--- lib/rust/hyeperbee.rs
+++ lib/rust/hyeperbee.rs
@@ -1,1 +1,26 @@
+use std::io::{Read, Write};
+use hypercore::Feed;
+use hyperbee::RAM;
 
+
+pub struct HyperbeeLibrary {
+    feed: Feed,
+    ram: Ram,
+}
+
+impl HypercoreLibrary {
+    pub fn new(feed: Feed) -> Self {
+        HypercoreLibrary { feed }
+    }
+
+    pub fn read_data(&mut self, start: usize, length: usize) -> Result<Vec<u8>, hypercore::Error> {
+        let mut buf = vec![0u8; length];
+        self.feed.read(start, &mut buf)?;
+        Ok(buf)
+    }
+
+    pub fn append_data(&mut self, data: &[u8]) -> Result<(), hypercore::Error> {
+        self.feed.append(data)?;
+        Ok(())
+    }
+}
